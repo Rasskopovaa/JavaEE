a4029aac3044e5853c9d8f7177e8c9c111cab9fb
Rasskopovaa <raskopovaa@gmail.com>
2020-01-09 01:21:29 +0700

    jenerics2

---
+++ src/main/java/Lesson4/Course/third/Line.java
@@ -0,0 +1,7 @@
+package Lesson4.Course.third;
+
+public class Line {
+    public void draw(){
+        System.out.println("Line drawing");
+    }
+}
---
+++ src/main/java/Lesson4/Course/third/Poligon.java
@@ -0,0 +1,7 @@
+package Lesson4.Course.third;
+
+public class Poligon extends Line {
+    public void draw(){
+        System.out.println("Poligon drawing");
+    }
+}
