d52b40b460fe9761caac036ce4529e37c058c7eb
Rasskopovaa <rask@users.noreply.github.com>
2020-03-07 00:00:21 +0700

    sockets, общение сервера с клиентом асинхронное

---
+++ Sockets/src/main/java/Lesson9/Course/Server/ServerListener.java
@@ -0,0 +1,25 @@
+package Lesson9.Course.Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListener extends Thread {
+    @Override
+    public void run() {
+        try {
+            ServerSocket clientSocket = new ServerSocket(Server.SERVER_PORT);
+            Socket socket = clientSocket.accept();
+            InputStream fromClient = socket.getInputStream();
+
+            BufferedReader clientReader = new BufferedReader(new InputStreamReader(fromClient));
+            String message = null;
+            while ((message = clientReader.readLine()) != null) {
+                System.out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
